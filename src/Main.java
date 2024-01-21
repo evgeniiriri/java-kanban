0540da2da2bb329b1f366d00d8c728903655abb4
evgeniiriri <evgenii.ridberg@yandex.ru>
2024-01-21 21:59:07 +0300

    <feat> добавил InMemoryHistoryManager.java. Хранит 10 последних изменений. <fix> InMemoryTaskManager.java исправил методы обновления. Теперь они не создают дубликаты Task, Epic, Subtasks. <test> добавил тесты.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import kanban.model.Epic;
-import kanban.model.Status;
 import kanban.model.Subtask;
 import kanban.model.Task;
 import kanban.service.Manager;
@@ -10,9 +9,11 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Поехали!");
-
         TaskManager manager = Manager.getDefault();
+        printAllTasks(manager);
+    }
 
+    private static void printAllTasks(TaskManager manager) {
         Task testTask1 = new Task("Кот", "Покормить, напоить.");
         Task testTask2 = new Task("Попугай", "Покормить, напоить.");
         Epic testEpic1 = new Epic("Зарядка" , "...");
@@ -21,6 +22,37 @@ public class Main {
         Subtask testSubtask2 = new Subtask("Прец качат", "100 раз");
         Subtask testSubtask3 = new Subtask("Бегит", "5 км");
         Subtask testSubtask4 = new Subtask("Молоко", "3.2%");
+
+        manager.createTask(testTask1);
+        manager.createTask(testTask2);
+        manager.createEpic(testEpic1);
+        manager.createEpic(testEpic2);
+        manager.createSubTask(testSubtask1, testEpic1);
+        manager.createSubTask(testSubtask2, testEpic2);
+        manager.createSubTask(testSubtask3, testEpic2);
+        manager.createSubTask(testSubtask4, testEpic2);
+
+        System.out.println("Задачи:");
+        for (Task task : manager.getAllTask()) {
+            System.out.println(task);
+        }
+        System.out.println("Эпики:");
+        for (Task epic : manager.getAllEpic()) {
+            System.out.println(epic);
+
+            for (Task task : manager.getAllSubTask(epic.getId())) {
+                System.out.println("--> " + task);
+            }
+        }
+        System.out.println("Подзадачи:");
+        for (Task subtask : manager.getAllSubTask()) {
+            System.out.println(subtask);
+        }
+
+        System.out.println("История:");
+        for (Task task : manager.getHistory()) {
+            System.out.println(task);
+        }
     }
 
 }
